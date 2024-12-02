ca6b90b3b9a5bd9258bffb186ea76055ab0f4d37
Soumik-0406 <124161684+Soumik-0406@users.noreply.github.com>
2024-12-02 13:41:34 +0530

    Add files via upload

---
+++ Hangman Game/word-list.js
@@ -0,0 +1,83 @@
+const wordList = [
+    {
+        word: "candle",
+        Hint: "I'm tall when I'm young, and short when I'm old. What am I?"
+    },
+    { 
+
+        word: "piano",
+        Hint: "What has keys but can't open locks?"
+    },
+    {
+        word: "raindrop",
+        Hint: " I'm round, but I'm not a circle. I'm made of water, but I'm not a river. What am I?"
+    },
+    {
+        word: "sponge",
+        Hint: "I am full of holes but still hold water. What am I?"
+    },
+    {
+        word: "light",
+        Hint: "I start in a place but never move. I fill a room but take up no space. What am I?"
+    },
+    {
+        word: "fire",
+        Hint: "I am not alive, but I can grow. I don't have lungs, but I need air. What am I?"
+    },
+    {
+        word: "map",
+        Hint: " I have cities but no houses, forests but no trees, and rivers but no water. What am I?"
+    },
+    {
+        word: "water",
+        Hint: "I'm often running, but I never get tired. What am I?"
+    },
+    {
+        word: "footsteps",
+        Hint: "The more you take, the more you leave behind. What am I?"
+    },
+    {
+        word: "panda",
+        Hint: "I'm black and white and loved all over the world. What am I?"
+    },
+    {
+        word: "cloud",
+        Hint: " I can fly without wings. I can cry without eyes. Wherever I go, darkness follows me. What am I?"
+    },
+    {
+        word: "mirror",
+        Hint: "I look at you, you look at me, I raise my right, you raise your left. What am I?"
+    },
+    {
+        word: "mountain",
+        Hint: " I'm not a plant, but I can grow; I'm not alive, but I can die. What am I?"
+    },
+    {
+        word: "bottle",
+        Hint: "What has a neck but no head?"
+    },
+    {
+        word: "joke",
+        Hint: " I can be cracked, made, told, and played. What am I?"
+    },
+    {
+        word: "wool",
+        Hint: "I'm found in socks, scarves, and sweaters. I keep you warm in cold weather. What am I?"
+    },
+    {
+        word: "donut",
+        Hint: "I am round like a letter 'O', and the center is hollow. What am I?"
+    },
+    {
+        word: "few",
+        Hint: " I'm a word of letters three, add two and fewer there will be. What am I?"
+    },
+    {
+        word: "coin",
+        Hint: "What has a head, a tail, but no body?"
+    },
+    {
+        word: "future",
+        Hint: "I'm always in front of you, but you can never see me. What am I?"
+    },
+];
